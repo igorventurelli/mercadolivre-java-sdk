7700f009946ad3a786d7fa0420a042151ac67e49
Igor Venturelli <igor.venturelli@me.com>
2018-05-15 18:14:20 -0300

    Adding the Java project

---
+++ src/main/java/br/com/igorventurelli/App.java
@@ -0,0 +1,13 @@
+package br.com.igorventurelli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
