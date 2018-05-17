c38e4373d49f5e5875d1dd45ee69884ff8cf2f6a
Igor Venturelli <igor.venturelli@me.com>
2018-05-17 11:28:04 -0300

    Adding CodeClimate support

--- src/test/java/br/com/igorventurelli/mercadolivre/MercadoLivreTest.java
+++ src/test/java/br/com/igorventurelli/mercadolivre/MercadoLivreTest.java
@@ -4,6 +4,11 @@ import org.junit.Test;
 
 public class MercadoLivreTest {
 
+      @Test
+      public void shouldWorkd() {
+          System.out.println("It works!");
+      }
+
 //    @Test
 //    public void shouldWorks() {
 //        new MercadoLivre(4612445084919438L, "sR5OtmKoDfAeOTYfdhrSUNRtGp3N4Ip2");
